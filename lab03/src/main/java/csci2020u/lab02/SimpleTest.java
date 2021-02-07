73e6b05ec43de12549f001c8cdc55333204ee111
benjamin-tsoumagas <benjamin.tsoumagas@ontariotechu.net>
2021-02-07 14:36:10 -0500

    Lab 03 Finished

---
+++ lab03/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,9 @@
+package csci2020u.lab02;
+
+public class SimpleTest{
+    String x = "Hello world!";
+
+public static void main(String[] args){
+    System.out.println(new SimpleTest().x);
+    }
+}
