94e8ebfb47a1a84b012849f1f9bcee73a4546e8c
tsoumagas-benjamin <bptsoumagas@gmail.com>
2021-03-29 20:05:08 -0400

    Lab 09 Complete

--- lab09/src/sample/Controller.java
+++ lab09/src/sample/Controller.java
@@ -1,12 +1,54 @@
 package sample;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.ArrayList;
 
 public class Controller {
 
+    //Attributes used in modifying the target stock URL
+    static final private String interval = "1mo";
+    static final private String event = "history";
+    static final private boolean adjClose = true;
+    private static final long  start = LocalDate.of(2010,1,1)
+            .atStartOfDay(ZoneId.systemDefault()).toEpochSecond();
+    private static final long end = LocalDate.of(2015,12,31)
+            .atStartOfDay(ZoneId.systemDefault()).toEpochSecond();
 
-    private void downloadStockPrices(){
+    public static double[] downloadStockPrices(String stockTicker){
+        ArrayList<String> output = new ArrayList<>();
 
+        try {
+            URL url = new URL("https://query1.finance.yahoo.com/v7/finance/download/" + stockTicker + "?period1=" + start + "&period2=" + end + "&interval="+interval+"&events="+event+"&includeAdjustedClose="+adjClose);
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(false);
+            conn.setDoInput(true);
+            BufferedReader read = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            //Reads each line of the CSV at the URL and creates a String arraylist
+            //String arraylist contains 4th column a.k.a. close values
+            String line;
+            while((line = read.readLine()) != null){
+                String[] data;
+                data = line.split(",");
+                output.add(data[4]);
+            }
+            //Close reader and remove first value ('close' header)
+            read.close();
+            output.remove(0);
+            //Convert String arraylist to double array
+            double[] result = new double[output.size()];
+            for(int i = 0; i < output.size(); i++){
+                result[i] = Double.parseDouble(output.get(i));
+            }
+            return result;
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- lab09/src/sample/Main.java
+++ lab09/src/sample/Main.java
@@ -1,60 +1,77 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.Buffer;
-import java.time.LocalDate;
-import java.time.ZoneId;
 
 public class Main extends Application {
 
+    //Attributes for stage dimensions and base max value of the plot
+    static int height = 600;
+    static int width = 1000;
+    static double max = 0;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-
-        final String stockTicker = "GOOG";
-        final String stockTicker2 = "AAPL";
-        final String interval = "1mo";
-        long start = LocalDate.of(2020,1,1)
-                .atStartOfDay(ZoneId.systemDefault()).toEpochSecond();
-        long end = LocalDate.of(2021,3,24)
-                .atStartOfDay(ZoneId.systemDefault()).toEpochSecond();
-
-        System.out.println("https://query1.finance.yahoo.com/v7/finance/download/"+stockTicker+"?period1="+start+"&period2="+end+"&interval="+interval+"&events=history&includeAdjustedClose=true");
-
-        try {
-            URL url = new URL("https://query1.finance.yahoo.com/v7/finance/download/" + stockTicker + "?period1=" + start + "&period2=" + end + "&interval=1mo&events=history&includeAdjustedClose=true");
-            URLConnection conn = url.openConnection();
-            conn.setDoOutput(false);
-            conn.setDoInput(true);
-            BufferedReader read = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            //StringBuilder -> List
-            StringBuilder lines = new StringBuilder();
-            String line;
-            while((line = read.readLine()) != null){
-                lines.append(line);
-                lines.append("\n");
-            }
-            read.close();
-            System.out.println(lines.toString());
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+    public void start(Stage primaryStage) {
+        //Set up a new canvas for GraphicsContext to draw on
+        Group solution = new Group();
+        Scene lineChart = new Scene(solution, width, height);
+        Canvas lineCanvas = new Canvas(width, height);
+        solution.getChildren().add(lineCanvas);
+        //Instantiate a GraphicsContext object
+        GraphicsContext gc = lineCanvas.getGraphicsContext2D();
+        //Call drawLinePlot with the outputs of downloadStockPrices (double[]) as arguments
+        drawLinePlot(gc, Controller.downloadStockPrices("GOOG"), Controller.downloadStockPrices("AAPL"));
+        primaryStage.setTitle("Lab 09: Stock Performance");
+        primaryStage.setScene(lineChart);
 
         primaryStage.show();
     }
 
+    public static void drawLinePlot(GraphicsContext gc, double[] listA, double[] listB){
 
+        //Determine max and min values for the plot
+        for (double value : listA){
+            if(value > max)
+                max = value;
+        }
+        for (double value : listB){
+            if(value > max)
+                max = value;
+        }
+
+        //draw x and y axis 50px from bottom left edge
+        gc.setLineWidth(1);
+        gc.setFill(Color.BLACK);
+        gc.setStroke(Color.BLACK);
+        ///x-axis
+        gc.strokeLine(50,height-50,width-50,height-50);
+        //y-axis
+        gc.strokeLine(50,height-50,50,50);
+        //call plotLine twice (once per stock)
+        plotLine(gc, listA, Color.RED);
+        plotLine(gc, listB, Color.BLUE);
+    }
+
+    public static void plotLine(GraphicsContext gc, double[] list, Color colour){
+        gc.setLineWidth(1);
+        gc.setFill(colour);
+        gc.setStroke(colour);
+        //Number of values to plot
+        int length = list.length;
+        //The vertical/horizontal distance between each plotted point
+        double widthInterval = (width - 100.0) / length;
+        double heightInterval = ((100.0 - height)/-max);
+        //Plot lines from each current point to the next, iterate over number of values
+        for(int i = 0; i < length-1; i++){
+            gc.strokeLine(50+i*widthInterval, height-50-heightInterval*list[i], 50+(i+1)*widthInterval, height-50-heightInterval*list[i+1]);
+        }
+    }
+    
     public static void main(String[] args) {
         launch(args);
     }
